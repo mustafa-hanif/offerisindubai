b282f7084488005d8c9869338c3ef6045bd37000
mustafa-hanif <mustafa.hanif@dubizzle.com>
2021-11-11 11:30:05 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-xgay38-n9hubw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-xgay38-n9hubw)
---
+++ pages/index.js
@@ -0,0 +1,128 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Offers in Dubai</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-4 text-center">
+        <Heading />
+        <Example />
+      </main>
+    </div>
+  );
+}
+
+/*
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
+  ```
+*/
+const files = [
+  {
+    title: 'IMG_4985.HEIC',
+    size: '3.9 MB',
+    source:
+      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+  },
+  {
+    title: 'IMG_4985.HEIC',
+    size: '3.9 MB',
+    source:
+      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+  },
+  {
+    title: 'IMG_4985.HEIC',
+    size: '3.9 MB',
+    source:
+      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+  },
+  {
+    title: 'IMG_4985.HEIC',
+    size: '3.9 MB',
+    source:
+      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+  },
+  {
+    title: 'IMG_4985.HEIC',
+    size: '3.9 MB',
+    source:
+      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+  },
+  {
+    title: 'IMG_4985.HEIC',
+    size: '3.9 MB',
+    source:
+      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+  },
+  // More files...
+];
+
+function Example() {
+  return (
+    <ul
+      role="list"
+      className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
+    >
+      {files.map((file) => (
+        <li key={file.source} className="relative">
+          <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
+            <img
+              src={file.source}
+              alt=""
+              className="object-cover pointer-events-none group-hover:opacity-75"
+            />
+            <button
+              type="button"
+              className="absolute inset-0 focus:outline-none"
+            >
+              <span className="sr-only">View details for {file.title}</span>
+            </button>
+          </div>
+          <p className="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">
+            {file.title}
+          </p>
+          <p className="block text-sm font-medium text-gray-500 pointer-events-none">
+            {file.size}
+          </p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function Heading() {
+  return (
+    <div className="bg-white px-4 py-5 border-gray-200 sm:px-6">
+      <div className="-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-nowrap">
+        <div className="ml-4 mt-2">
+          <h3 className="text-lg leading-6 font-medium text-gray-900">
+            Job Postings
+          </h3>
+        </div>
+        <div className="ml-4 mt-2 flex-shrink-0">
+          <button
+            type="button"
+            className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Create new job
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
