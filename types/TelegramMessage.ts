9875569ebf9e7bcb55d404a6219137515cdca6b4
Mustafa Hanif <mustafa.hanif@seera.sa>
2021-11-20 19:23:51 +0400

    Add all necessary stuff

---
+++ types/TelegramMessage.ts
@@ -0,0 +1,31 @@
+export interface TelegramMessage {
+  update_id: number;
+  message: {
+    message_id: number;
+    from: {
+      id: number;
+      is_bot: boolean;
+      first_name: string;
+      last_name: string;
+      username: string;
+      language_code: string;
+    };
+    chat: {
+      id: number;
+      first_name: string;
+      last_name: string;
+      username: string;
+      type: string;
+    };
+    date: number;
+    text: string;
+    photo?: [
+      {
+        file_id: string;
+        file_size: number;
+        width: number;
+        height: number;
+      }
+    ];
+  };
+};
